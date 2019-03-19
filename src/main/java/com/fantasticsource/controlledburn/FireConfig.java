0a6d0fd98235d26707f3952f6540808109bfab71
Kyle Koder <silvertallon@gmail.com>
2019-03-18 20:36:26 -0700

    Added lang file and lang keys for all config settings

--- src/main/java/com/fantasticsource/controlledburn/FireConfig.java
+++ src/main/java/com/fantasticsource/controlledburn/FireConfig.java
@@ -7,23 +7,34 @@ import net.minecraftforge.common.config.Config.Comment;
 public class FireConfig
 {
     @Config.Name("Global Multipliers")
+    @Config.LangKey(ControlledBurn.MODID + ".config.globalMultipliers")
     @Comment({
             "Multipliers for how fast fire naturally spreads and how fast fire burns blocks",
             "",
             "Also includes the setting for how often fire updates (which makes everything fire-related faster or slower)"
     })
     public static GlobalMultipliers globalMultipliers = new GlobalMultipliers();
+
+    @Config.LangKey(ControlledBurn.MODID + ".config.specialToggles")
     @Comment({"Includes whether certain sources can start fires, whether fire ignores rain, and other oddities"})
     public static SpecialToggles specialToggles = new SpecialToggles();
+
+    @Config.LangKey(ControlledBurn.MODID + ".config.burnSpreadChances")
     @Comment({"These determine how often fire spreads to adjacent blocks after burning a block"})
     public static BurnSpreadChances burnSpreadChances = new BurnSpreadChances();
+
     @Config.Name("Natural Fire Spread Ranges")
+    @Config.LangKey(ControlledBurn.MODID + ".config.spreadRanges")
     @Comment({"How far fire can naturally spread from one block to another in each direction"})
     public static FireSpreadRanges spreadRanges = new FireSpreadRanges();
+
     @Config.Name("Spread Strengths")
+    @Config.LangKey(ControlledBurn.MODID + ".config.spreadStrengths")
     @Comment({"How much 'life' new fires have when spreading"})
     public static SpreadStrengths spreadStrengths = new SpreadStrengths();
+
     @Config.Name("Block-Specific Settings")
+    @Config.LangKey(ControlledBurn.MODID + ".config.blockSpecific")
     @Comment({
             "This allows you to set a custom BASE flammability and encouragement for any given block",
             "",
@@ -44,6 +55,7 @@ public class FireConfig
     public static class GlobalMultipliers
     {
         @Config.Name("Burn Speed Multiplier")
+        @Config.LangKey(ControlledBurn.MODID + ".config.burnSpeedMultiplier")
         @Comment({
                 "How fast blocks are destroyed (burnt) when 'on fire', as a multiplier (2 means twice as fast, 0.5 means half as fast)",
                 "",
@@ -55,6 +67,7 @@ public class FireConfig
         public double burnSpeedMultiplier = 1;
 
         @Config.Name("Spread Speed Multiplier")
+        @Config.LangKey(ControlledBurn.MODID + ".config.spreadSpeedMultiplier")
         @Comment({
                 "How fast fire spreads, as a multiplier (2 means twice as fast, 0.5 means half as fast)",
                 "",
@@ -64,6 +77,7 @@ public class FireConfig
         public double spreadSpeedMultiplier = 1;
 
         @Config.Name("Tick Delay")
+        @Config.LangKey(ControlledBurn.MODID + ".config.tickDelay")
         @Comment({
                 "The number of game ticks before fire blocks update themselves",
                 "",
@@ -76,6 +90,7 @@ public class FireConfig
     public static class SpecialToggles
     {
         @Config.Name("Ignore Humid Biomes")
+        @Config.LangKey(ControlledBurn.MODID + ".config.ignoreHumid")
         @Comment({
                 "If set to true, fire ignores the fire resistance of humid biomes",
                 "",
@@ -84,6 +99,7 @@ public class FireConfig
         public boolean ignoreHumidBiomes = false;
 
         @Config.Name("Ignore Rain")
+        @Config.LangKey(ControlledBurn.MODID + ".config.ignoreRain")
         @Comment({
                 "If set to true, fire ignores rain",
                 "",
@@ -92,10 +108,12 @@ public class FireConfig
         public boolean ignoreRain = false;
 
         @Config.Name("Disable Fires From Lava")
+        @Config.LangKey(ControlledBurn.MODID + ".config.lavaFire")
         @Comment({"Whether fire spreading from lava is disabled"})
         public boolean noLavaFire = false;
 
         @Config.Name("Disable Fires From Lightning")
+        @Config.LangKey(ControlledBurn.MODID + ".config.lightningFire")
         @Comment({"Whether fire spreading from lightning is disabled"})
         public boolean noLightningFire = false;
     }
@@ -103,6 +121,7 @@ public class FireConfig
     public static class BurnSpreadChances
     {
         @Config.Name("Max Burn Spread Chance")
+        @Config.LangKey(ControlledBurn.MODID + ".config.maxBurnSpreadChance")
         @Comment({
                 "When a fire at the MAXIMUM age (15 in vanilla) destroys (burns through) a block, the chance that the broken (burnt) block is replaced with fire",
                 "",
@@ -114,6 +133,7 @@ public class FireConfig
         public int maxBurnSpreadChance = 80;
 
         @Config.Name("Min Burn Spread Chance")
+        @Config.LangKey(ControlledBurn.MODID + ".config.minBurnSpreadChance")
         @Comment({
                 "When a fire at the MINIMUM age (0 in vanilla) destroys (burns through) a block, the chance that the broken (burnt) block is replaced with fire",
                 "",
@@ -128,16 +148,19 @@ public class FireConfig
     public static class FireSpreadRanges
     {
         @Config.Name("Reach (Upwards)")
+        @Config.LangKey(ControlledBurn.MODID + ".config.reachUp")
         @Comment({"The maximum upward distance fire can naturally spread to when updating (eg. between two trees)"})
         @Config.RangeInt(min = 0)
         public int reachAbove = 4;
 
         @Config.Name("Reach (Downwards)")
+        @Config.LangKey(ControlledBurn.MODID + ".config.reachDown")
         @Comment({"The maximum downward distance fire can naturally spread to when updating (eg. between two trees)"})
         @Config.RangeInt(min = 0)
         public int reachBelow = 1;
 
         @Config.Name("Reach (Sideways)")
+        @Config.LangKey(ControlledBurn.MODID + ".config.reachSide")
         @Comment({"The maximum horizontal distance fire can naturally spread to when updating (eg. between two trees)"})
         @Config.RangeInt(min = 0)
         public int reachHorizontal = 1;
@@ -146,6 +169,7 @@ public class FireConfig
     public static class SpreadStrengths
     {
         @Config.Name("Spread Strength (Block Burning)")
+        @Config.LangKey(ControlledBurn.MODID + ".config.burnSpreadStrength")
         @Comment({
                 "When fire spreads by destroying a block, the new fire's duration is set to the duration of the fire it came from, multiplied by this",
                 "",
@@ -159,6 +183,7 @@ public class FireConfig
         public int burnSpreadStrength = -1;
 
         @Config.Name("Spread Strength (Natural Spread)")
+        @Config.LangKey(ControlledBurn.MODID + ".config.naturalSpreadStrength")
         @Comment({
                 "When fire spreads naturally, ie. not when it's spreading by destroying (burning) a block, the new fire's duration is set to the duration of the fire it came from, multiplied by this",
                 "",
