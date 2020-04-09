6838d1f3b308630f7ec24ec13e61f9c03e5347ed
Kyle Koder <silvertallon@gmail.com>
2020-04-09 15:13:36 -0700

    1.12.2.019

--- src/main/java/com/fantasticsource/controlledburn/ControlledBurn.java
+++ src/main/java/com/fantasticsource/controlledburn/ControlledBurn.java
@@ -27,7 +27,7 @@ public class ControlledBurn
 {
     public static final String MODID = "controlledburn";
     public static final String NAME = "Controlled Burn";
-    public static final String VERSION = "1.12.2.018";
+    public static final String VERSION = "1.12.2.019";
     public static int replaceBlockWithFireChanceRange;
     public static BlockFire oldFire;
 
